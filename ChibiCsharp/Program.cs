5b76f3bb917e0ebe6565f0520271ab0e4c546f55
binnmti <binnmtiisprogrammer@gmail.com>
2025-01-15 11:31:26 +0900

    Add ChibiCsharp And Test

---
+++ ChibiCsharp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ChibiCsharp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if(args.Length != 1)
+        {
+            Console.WriteLine("Hello, World!");
+            return;
+        }
+        Console.WriteLine(".assembly AddExample { }");
+        Console.WriteLine(".method static void Main() cil managed {");
+        Console.WriteLine("\t.entrypoint");
+        Console.WriteLine($"\tldc.i4 {args[0]}");
+        Console.WriteLine("\tcall void [mscorlib]System.Console::WriteLine(int32)");
+        Console.WriteLine("\tret");
+        Console.WriteLine("}");
+    }
+}
