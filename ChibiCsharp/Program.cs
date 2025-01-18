e0d7b3b3af7bfc2e80ea22d967fc9b4d6e629e20
binnmti <binnmtiisprogrammer@gmail.com>
2025-01-18 15:17:48 +0900

    ステップ2：加減算のできるコンパイラの作成 準備

--- ChibiCsharp/Program.cs
+++ ChibiCsharp/Program.cs
@@ -11,7 +11,31 @@ internal class Program
         }
         Console.WriteLine(".assembly AddExample { }");
         Console.WriteLine(".method static void Main() cil managed {");
-        Console.WriteLine("\t.entrypoint");
+        Console.WriteLine(" .entrypoint");
+
+        foreach(var s in args[0])
+        {
+            if(s == '+')
+            {
+                Console.WriteLine($"    add");
+            }
+            else if(s == '-')
+            {
+                Console.WriteLine($"    sub");
+            }
+
+        }
+
+
+        /*
+        ldc.i4 5
+        ldc.i4 20
+        add
+        ldc.i4 4
+        sub         * 
+         */
+
+
         Console.WriteLine($"\tldc.i4 {args[0]}");
         Console.WriteLine("\tcall void [mscorlib]System.Console::WriteLine(int32)");
         Console.WriteLine("\tret");
