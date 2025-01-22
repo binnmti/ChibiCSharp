9699caa45a870399808c5e3abfd9319f20bc2421
binnmti <binnmtiisprogrammer@gmail.com>
2025-01-23 06:51:41 +0900

    Compiler.Compile

---
+++ ChibiCSharpCompiler/Compiler.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace ChibiCSharpCompiler;
+
+public class Compiler
+{
+    public static string Compile(string arg)
+    {
+        var result = new StringBuilder();
+        result.AppendLine(".assembly AddExample { }");
+        result.AppendLine(".method static void Main() cil managed {");
+        result.AppendLine("    .entrypoint");
+        for (int i = 0; i < arg.Length; i++)
+        {
+            char c = arg[i];
+            if (c == '+')
+            {
+                var (str, idx) = GetNumber(arg, i + 1);
+                i = idx;
+                result.AppendLine($"    ldc.i4 {str}");
+                result.AppendLine($"    add");
+            }
+            else if (c == '-')
+            {
+                var (str, idx) = GetNumber(arg, i + 1);
+                i = idx;
+                result.AppendLine($"    ldc.i4 {str}");
+                result.AppendLine($"    sub");
+            }
+            else
+            {
+                var (str, idx) = GetNumber(arg, i);
+                i = idx;
+                result.AppendLine($"    ldc.i4 {str}");
+            }
+        }
+        result.AppendLine("    call void [mscorlib]System.Console::WriteLine(int32)");
+        result.AppendLine("    ret");
+        result.AppendLine("}");
+        return result.ToString();
+    }
+
+    private static (string, int) GetNumber(string arg, int idx)
+    {
+        var s = arg[idx].ToString();
+        while (true)
+        {
+            if (idx + 1 == arg.Length) break;
+            var c = arg[idx + 1];
+            if (!char.IsNumber(c)) break;
+            s = s + c;
+            idx++;
+        }
+        return (s, idx);
+    }
+}
--- ChibiCsharp/Program.cs
+++ ChibiCsharp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ChibiCsharp;
+﻿using ChibiCSharpCompiler;
+
+namespace ChibiCSharp;
 
 internal class Program
 {
@@ -9,49 +11,7 @@ internal class Program
             Console.WriteLine("Hello, World!");
             return;
         }
-        Console.WriteLine(".assembly AddExample { }");
-        Console.WriteLine(".method static void Main() cil managed {");
-        Console.WriteLine(" .entrypoint");
-        for (int i = 0; i < args[0].Length; i++)
-        {
-            char c = args[0][i];
-            if (c == '+')
-            {
-                var (str, idx) = GetNumber(args[0], i + 1);
-                i = idx;
-                Console.WriteLine($"    ldc.i4 {str}");
-                Console.WriteLine($"    add");
-            }
-            else if (c == '-')
-            {
-                var (str, idx) = GetNumber(args[0], i + 1);
-                i = idx;
-                Console.WriteLine($"    ldc.i4 {str}");
-                Console.WriteLine($"    sub");
-            }
-            else
-            {
-                var (str, idx) = GetNumber(args[0], i);
-                i = idx;
-                Console.WriteLine($"    ldc.i4 {str}");
-            }
-        }
-        Console.WriteLine("    call void [mscorlib]System.Console::WriteLine(int32)");
-        Console.WriteLine("    ret");
-        Console.WriteLine("}");
-    }
-
-    private static (string, int) GetNumber(string arg, int idx)
-    {
-        var s = arg[idx].ToString();
-        while (true)
-        {
-            if (idx + 1 == arg.Length) break;
-            var c = arg[idx + 1];
-            if (!char.IsNumber(c)) break;
-            s = s + c;
-            idx++;
-        }
-        return (s, idx);
+        var result = Compiler.Compile(args[0]);
+        Console.WriteLine(result);
     }
 }
